35bea78b776be30a30a71dee2d931ed3de8d5447
Craig Harshbarger <pleek91@gmail.com>
2024-03-23 11:57:10 -0500

    scaffolding

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  test: {
+    typecheck: {
+      checker: 'vue-tsc',
+      ignoreSourceErrors: true,
+    },
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src'),
+      },
+    ],
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: '@kitbag/query',
+      fileName: 'kitbag-query',
+    },
+  },
+  plugins: [
+    vue(),
+    dts({ 
+      rollupTypes: true 
+    })
+  ],
+  rollupOptions: {
+    external: ['vue'],
+    output: {
+      globals: {
+        vue: 'Vue',
+      },
+    },
+  },
+})
